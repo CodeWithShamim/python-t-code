299811ac33a098dc0353ecebf024ad298de12bff
codewithshamim <shamimislamonline@gmail.com>
2021-09-19 21:59:43 +0600

    Update

--- Class and object, (OOP),  Language.py
+++ Class and object, (OOP),  Language.py
@@ -2,13 +2,22 @@ class student:
      roll : ""
      gpa : ""
 
+
+
+'''
+#Check bolean value..
+check = isinstance(wazed, student)
+print(check)
+'''
 wazed = student()
 wazed.roll = 101
 wazed.gpa = 5.00
 
 alamin = student()
 alamin.roll = 102
-alamin.gpa = 5.00
+alamin.gpa = 4.00
 
 print(f" Roll : {wazed.roll}, GPA : {wazed.gpa}")
 print(f" Roll : {alamin.roll}, GPA : {alamin.gpa}")
+
+
--- Exception Handling.py
+++ Exception Handling.py
@@ -1,31 +1,67 @@
+#Method 11
 try:
     list1 = [10, 20, 30]
-    list2 = [0, 50, 60]
+    list2 = [0, 5, 50, 60]
 
     result = list1[0] / list2[0]
-
+    print(result)
 except ZeroDivisionError:
-     print("Some problem!!")
-
-
+     print("Last : Some problem!!")
 finally:
-      print("Successful")
+      print("Done\n\n")
 
 
-#print type error problem........input string value...
+#Method 22
 try:
-    num = input("Enter your number : ")
+    list1 = [10, 20, 30]
+    list2 = [0, 5, 50, 60]
 
-    result = 50 / num
+    result = list1[0] / list2[1]
+    print("Again : ", result)
 
-except TypeError:
+except IndentationError:
      print("Some problem!!")
+
+#finally always print...................
+finally:
+      print("Value !!! Successful\n\n")
+
+
+
+#-------------------------------------------------------------------------------------
+
+try:
+    list1 = [10, 20, 30]
+    list2 = [0, 5, 50, 60]
+
+    #error-1
+    result = list1[0] / list2[0]
+    print("Again : ", result)
+
+    #error-1
+    list = ["shamim"]
+    resulst = list[7]
+    print(result)
+
+except (IndexError, ValueError, SyntaxError, TypeError, ZeroDivisionError, IndentationError):
+     print("You have entered incorrect input!!")
 
 
 finally:
-      print("Successful")
+      print("Successful!!!")
+#---------------------------------------------------------------------------------------------
 
+def voter(age):
+    if age<18:
+       raise ValueError("Invalid voter&&&.")
 
+    return "You are allowed to vote."
 
+try:
+    
+    print(voter(19))
+    print(voter(17))
 
-
+#Use as change error name....
+except ValueError as v:
+    print(v)
